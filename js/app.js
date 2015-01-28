f5c8b3e71670ff6efc140a9710f29111c9d037d3
Chris Rempola <chrisxstyles@gmail.com>
2015-01-27 20:09:15 -0800

    Adding a maxNum function to select highest num from random and user

--- js/app.js
+++ js/app.js
@@ -12,19 +12,18 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	//alert("new num is " + generateNum());
+  	var randomNum = generateNum();
+  	alert("random num is " + randomNum);
 
   	counter = 0;
   	$("form").submit(function(e){
-  		var num = getNum();
-  		checkNum(num);
+  		var userNum = getNum();
+  		checkNum(randomNum, userNum);
   		counter++;
   		$("#count").text(counter);
   		$("#userGuess").attr("placeholder", "Enter your Guess").val("").focus().blur();
   		//return false;
   		e.preventDefault();
-
-  		
   	});
 
   	newGame();
@@ -37,12 +36,20 @@ $(document).ready(function(){
   	};
 
   	// Make sure number is a whole number
-  	function checkNum(n){
+  	function checkNum(r, n){
   		if (n % 1 != 0){
   			$("#feedback").text("The number: " +n +" is not a whole number");
   		}
-  		else { 
-  			$("#feedback").text("You entered " + n);
+  		else {
+  			var findMax = maxNum(r, n);
+  			var highNum = findMax[0];
+  			var lowNum  = findMax[1];
+  			if (highNum - lowNum >= 50){
+  				$("#feedback").text("You entered " +n +", You are COLD!");
+  			}
+  			else if (highNum - lowNum <= 20){ 
+  				$("#feedback").text("You entered " +n +", You are WARM!");
+  			}
   		};
   	};
 
@@ -56,6 +63,15 @@ $(document).ready(function(){
 		// random number between 1 and 100
 		var num = Math.floor((Math.random() * 100) + 1);
 		return num;
+	};
+
+	function maxNum(r, n){
+		if (Math.max(r, n) == r){
+			return [r, n];
+		}
+		if (Math.max(r, n) == n){
+			return [n, r];
+		}
 	};
 
 });
